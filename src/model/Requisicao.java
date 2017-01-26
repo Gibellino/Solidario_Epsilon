d11a8ba370c72c896ac64b2bb927affde34dfd77
Gibellino <bbgibellino@gmail.com>
2017-01-26 15:17:55 +0000

    Initial

---
+++ src/model/Requisicao.java
@@ -0,0 +1,15 @@
+package model;
+
+public class Requisicao {
+
+	public Requisicao() {
+		// TODO Auto-generated constructor stub
+		
+		int id_Requisicao;
+		IRequisitavel item;
+		Utilizador user;
+		
+		
+	}
+
+}
